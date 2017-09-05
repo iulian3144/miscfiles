8028ac52fdeac8d74c55ff71be31afbe47fd54ff
Iulian PAUN <pauniulian3144@gmail.com>
2017-09-05 23:42:51 +0300

    modify symtbl_test/main.c
    implement symbol lookup function and other things.

--- EmbeddedStuff/Arduino/barebone/symtbl_test/main.c
+++ EmbeddedStuff/Arduino/barebone/symtbl_test/main.c
@@ -1,24 +1,95 @@
 #include <stdlib.h>
 #include <stdio.h>
+#include <string.h>
 #include "symbol.h"
 
-
 extern SYMBOL standTbl[];
+extern ULONG standTblSize;
 
-int test = 2;
-char *hello()
+unsigned int running = 1;
+
+#define SHELL_CMD_LINE_SIZE 256
+
+void ls()
 {
-    return "hello";
+    printf("Listing directory contents...\n");
+}
+
+void hello()
+{
+    printf("hello\n");
+}
+
+void quit()
+{
+    running = 0;
+}
+
+void lkup(char *name)
+{
+    int i;
+    printf("Looking for %s\n", name);
+    printf("          Value      Name\n");
+    for(i = 0; i < standTblSize; ++i)
+    {
+        if(strcmp(name, standTbl[i].name) == 0)
+        {
+            printf("%p %10s\n", standTbl[i].value, standTbl[i].name);
+            continue;
+        }
+    }
+}
+
+static void shellGetCmd(char buffer[])
+{
+    char ch = 0;
+    unsigned int bytes_read = 0;
+    while(1)
+    {
+        if(bytes_read < SHELL_CMD_LINE_SIZE)
+            ch = getc(stdin);
+        if(ch == '\n')
+            return;
+        buffer[bytes_read++] = ch;
+    }
 }
 
 int main(int argc, char const *argv[])
 {
-    printf("hw\n");
-    printf("standTbl[0]->nameHNode: %p\n", standTbl[0].nameHNode);
-    printf("standTbl[0]->name     : %s\n", standTbl[0].name);
-    printf("standTbl[0]->value    : %p\n", standTbl[0].value);
-    printf("standTbl[0]->symRef   : %lu\n", standTbl[0].symRef);
-    printf("standTbl[0]->group    : %d\n", standTbl[0].group);
-    printf("standTbl[0]->type     : 0x%x\n", standTbl[0].type);
+    const char delim[] = " ";
+    char cmd_buf[SHELL_CMD_LINE_SIZE];
+    char *cmd;
+    void (*foo)(char *);
+    int i = 0;
+
+    memset(cmd_buf, 0, SHELL_CMD_LINE_SIZE);
+
+    while(running)
+    {
+        printf("> ");
+        shellGetCmd(cmd_buf);
+        // printf("%s\n", cmd_buf);
+        // return 1;
+
+        cmd = strtok(cmd_buf, delim);
+        for(i = 0; i < standTblSize; ++i)
+        {
+            if(cmd == 0)
+                break;
+            if(strcmp(cmd, standTbl[i].name) == 0)
+            {
+                foo = (void (*)(char *))standTbl[i].value;
+                foo(strtok(NULL, delim));
+                memset(cmd_buf, 0, SHELL_CMD_LINE_SIZE);
+                break;
+            }
+            if(i == (standTblSize - 1))
+            {
+                printf("Unknown command!\n");
+                break;
+            }
+        }
+    }
+
     return 0;
 }
